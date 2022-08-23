33941e55a86ffebfe454b991921059ed7240c689
John Bynum <iexclusiveproductions@gmail.com>
2022-08-23 18:46:52 -0400

    Added MIT License

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,28 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license == "MIT") {
+    return "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  } else if(license == "IBM") {
+    return "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)";
+  } else{
+    return ""
+  }
+}
+
+
+
 
 // TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) {}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) {
+
+// }
 
 // TODO: Create a function to generate markdown for README
 const generateMarkdown = (data) => {
@@ -19,7 +33,7 @@ const generateMarkdown = (data) => {
   ${data.Description}
   
   ## License
-  ${data.License}
+  ${renderLicenseBadge(data.License)}
 
   ## Table of Contents
   - [Description](#Description)
@@ -55,7 +69,7 @@ const generateMarkdown = (data) => {
   
   
   ## Help
-  `
+  If you require any assistance please send us an email and we will respond as soon as possible. `
 };
 
 
